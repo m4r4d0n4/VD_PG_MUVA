9777ec4d6878fdfa638e0b44348bbf490c820fae
Michael Jackson <58432330+m4r4d0n4@users.noreply.github.com>
2024-05-07 19:44:21 +0200

    feat(background): knn background added

--- demos/mog_background_subtraction.py
+++ demos/mog_background_subtraction.py
@@ -1,15 +1,27 @@
 import cv2
-
+import os,sys
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
 from src.background_subtraction.mog import MogBackgroundSubtraction
+from src.background_subtraction.knn import KNNBackgroundSubtraction
 from src.utils.background_subtraction import clean_image, remove_shadows
 from src.utils.video import get_next_frame
 
 
+
+
 REMOVE_SHADOWS = True
 REMOVE_NOISE = True
+BG_MOG = False
+BG_KNN = True
 VIDEO_PATH = "../resources/video/Walking.54138969.mp4"
 
-method = MogBackgroundSubtraction()
+if BG_MOG:
+    method = MogBackgroundSubtraction()
+elif BG_KNN:
+    method = KNNBackgroundSubtraction()
+
 
 for frame, fps in get_next_frame(VIDEO_PATH):
     foreground = method.apply(frame)
---
+++ src/background_subtraction/knn.py
@@ -0,0 +1,10 @@
+import cv2
+class KNNBackgroundSubtraction:
+    """
+    Class to handle KNN background subtraction on images.
+    """
+    def __init__(self):
+        self.knn = cv2.createBackgroundSubtractorKNN()
+
+    def apply(self, frame):
+        return self.knn.apply(frame)
