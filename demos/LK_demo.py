be39e2a28c90a2d1bfd54eb9ba31ea320c38e7e6
Michael Jackson <58432330+m4r4d0n4@users.noreply.github.com>
2024-05-26 20:36:07 +0200

    demos

--- demos/LK_demo.py
+++ demos/LK_demo.py
@@ -10,9 +10,11 @@ from src.techniques.particle_filter_wrapper import ParticleFilterWrapper
 from src.utils.background_subtraction import clean_image, remove_shadows
 from src.utils.video import get_next_frame
 from src.techniques.lucas_kanade import LucasKanadeTracker
+import time
+import statistics
 
 VIDEO_PATH = "./resources/video/Walking.60457274.mp4"
-
+MOSTRAR_IMAGENES = True
 
 def main():
     # Initialize ParticleFilter
@@ -21,6 +23,9 @@ def main():
     # Initialize background subtractor
     bg_subtraction = MogBackgroundSubtraction()
     init = False
+    # Inicializar el tiempo de inicio
+    start_time = time.time()
+    tiempos = []
     for frame, fps in get_next_frame(VIDEO_PATH):
         foreground = bg_subtraction.apply(frame)
         foreground = remove_shadows(foreground)
@@ -28,17 +33,31 @@ def main():
         #print(cv2.countNonZero(foreground))
         if not init and cv2.countNonZero(foreground) > 10000:
             lk.initialize(frame,foreground)
-            cv2.imshow("asd", foreground)
-            cv2.waitKey(0)
+            if MOSTRAR_IMAGENES:
+                cv2.imshow("Lucas-Kanade", foreground)
+                cv2.waitKey(0)
             init = True
         if init:
             #Para ver la caja
             f = lk.apply_bbox(frame)
             #Para el OF
             #f = lk.apply(frame)
-            cv2.imshow("Lucas-Kanade", f)
-            cv2.waitKey(0)
+            # Convertir el tiempo total de ejecución a milisegundos
+            total_time = (time.time() - start_time) * 1000
 
+            tiempos.append(total_time)
+            start_time = time.time()
+            #print("Tiempo total de ejecución:", total_time, "ms")
+            if MOSTRAR_IMAGENES:
+                cv2.imshow("Lucas-Kanade", f)
+                cv2.waitKey(int(fps))
+    # Calcular la media
+    media = statistics.mean(tiempos)
+
+    # Calcular la desviación estándar
+    desviacion_estandar = statistics.stdev(tiempos)
+    print("Media:", media)
+    print("Desviación estándar:", desviacion_estandar)
 
 if __name__ == "__main__":
     main()
--- demos/yolo_demo.py
+++ demos/yolo_demo.py
@@ -9,24 +9,40 @@ from src.techniques.particle_filter_v2 import ParticleFilterV2, disperse_motion_
 from src.techniques.particle_filter_wrapper import ParticleFilterWrapper
 from src.utils.video import get_next_frame
 from src.techniques.yolo_person_detector import YOLOPersonDetector
-
+import time
+import statistics
 VIDEO_PATH = "./resources/video/Walking.60457274.mp4"
-
+MOSTRAR_IMAGENES = True
 
 def main():
 
     # Crear una instancia del detector de personas
     detector = YOLOPersonDetector()
+    # Inicializar el tiempo de inicio
+    start_time = time.time()
+    tiempos = []
 
     for frame, fps in get_next_frame(VIDEO_PATH):
         # Aplicar la detección de personas
         frame, center_x,center_y, x1, y1, x2, y2, conf = detector.apply(frame)
+        # Convertir el tiempo total de ejecución a milisegundos
+        total_time = (time.time() - start_time) * 1000
 
-        # Mostrar el frame resultante (opcional)
-        cv2.imshow('Person Detection', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        tiempos.append(total_time)
+        start_time = time.time()
+        if MOSTRAR_IMAGENES:
+            # Mostrar el frame resultante (opcional)
+            cv2.imshow('Person Detection', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     
+    # Calcular la media
+    media = statistics.mean(tiempos)
+
+    # Calcular la desviación estándar
+    desviacion_estandar = statistics.stdev(tiempos)
+    print("Media:", media)
+    print("Desviación estándar:", desviacion_estandar)
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
